d48446f77822e6186b6dc07c347839cc1712d55b
Xr-VGoH <danielzhao001231@gmail.com>
2023-01-26 22:49:23 -0800

    new commit

--- src/components/Sections/Resume/Skills.tsx
+++ src/components/Sections/Resume/Skills.tsx
@@ -25,8 +25,8 @@ export const Skill: FC<{skill: SkillType}> = memo(({skill}) => {
   return (
     <div className="flex flex-col">
       <span className="ml-2 text-sm font-medium">{name}</span>
-      <div className="h-5 w-full overflow-hidden rounded-full bg-neutral-300">
-        <div className="h-full rounded-full bg-orange-400" style={{width: `${percentage}%`}} />
+      <div className="h-5 w-full overflow-hidden rounded-full bg-neutral-200">
+        <div className="h-full rounded-full bg-teal-600" style={{width: `${percentage}%`}} />
       </div>
     </div>
   );
